0fb051a352ac3b1c8e378e846b3942094c43b0d8
brook1711 <670396323@qq.com>
2018-04-22 13:48:14 +0800

    LinkStack

--- 链栈/链栈的实现.cpp
+++ 链栈/链栈的实现.cpp
@@ -44,21 +44,21 @@ LinkStack<T>::~LinkStack() //实现析构函数
 template<class T>
 void LinkStack<T>::Push(T x) //实现入栈操作
 {
-	Node<T> * p = new Node<T>;
-	p->data = x;
-	p->next = top;
-	top = p;
+	Node<T> * p = new Node<T>; //①	新建指针p指向申请的Node型堆空间。
+	p->data = x; //②	将新的节点中的data赋值为x。
+	p->next = top;//③	令新节点中的next指向栈顶元素。
+	top = p;//④	上移栈顶指针。
 }
 
 template<class T>
 T LinkStack<T>::Pop() //实现出栈操作
 {
-	if (Empty())
+	if (Empty()) //判断链表是否为空，若为空，抛出错误信息“下溢“。
 		throw"下溢";
-	T x = top->data;
-	Node<T> *p = top;
-	top = top->next;
-	delete p;
+	T x = top->data;//①	将栈顶元素的data储存在临时新建的T型变量x中。
+	Node<T> *p = top;//②	新建Node型指针指向栈顶元素。
+	top = top->next;//③	栈顶指针回溯。
+	delete p;//④	释放原栈顶指针，即p。
 	return x;
 }
 
